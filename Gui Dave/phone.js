005417821cf8a9f9a6e329db2a67543e06b6ad7b
Stonehenj <slapshotster@gmail.com>
2018-01-31 14:14:47 -0700

    finished assignment1

--- Gui Dave/phone.js
+++ Gui Dave/phone.js
@@ -1,1 +1,26 @@
-3
+$(document).ready(function() {
+	$("#content_dialer").show();
+	$("#content_add").hide();
+	$("#content_list").hide();
+});
+
+
+$("#tabDialer").click(function() {
+	$("#content_dialer").show();
+	$("#content_add").hide();
+	$("#content_list").hide();
+});
+
+
+$("#tabAdd").click(function() {
+    $("#content_add").show();
+    $("#content_dialer").hide();
+    $("#content_list").hide();
+});
+
+
+$("#tabList").click(function() {
+    $("#content_list").show();
+    $("#content_add").hide();
+    $("#content_dialer").hide();
+});
